18d1d2bedd845eee66731a4bad312c36afa496be
beomps5 <santiromerocontreiras@gmail.com>
2023-10-22 15:20:04 -0400

    listo

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import './style.css'
+fetch('https://leonardoapi.onrender.com/music')
+    .then((res) => res.json())
+    .then((resJson) => {
+        let tracklist = document.getElementById
+        ('track-list')
+        
+        resJson.map((song) => {
 
+            tracklist.innerHTML+=`
+            <div  class="box color">
+                    <img src="${song.path.front}" alt="">
+                    <h3>"${song.title}"</h3>
+                    <p>"${song.author}"</p>
+                  </div>
+            `
+
+
+        })
+        
+   })
