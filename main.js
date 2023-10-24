4159734bc52e5dc495912440f9509e84327be71b
beomps5 <santiromerocontreiras@gmail.com>
2023-10-24 19:54:40 -0400

    ahora si puedo dormir traqnuilo

--- main.js
+++ main.js
@@ -6,14 +6,29 @@ fetch('https://leonardoapi.onrender.com/music')
         ('track-list')
         
         resJson.map((song) => {
+            
 
-            tracklist.innerHTML+=`
-            <div  class="box color">
-                    <img src="${song.path.front}" alt="">
-                    <h3>"${song.title}"</h3>
-                    <p>"${song.author}"</p>
-                  </div>
+            const div = document.createElement('div')
+            div.classList.add(
+                'box',
+                'color'
+            )
+            div.innerHTML = `
+                <img src="${song.path.front}" alt="">
+                <h3>"${song.title}"</h3>
+                <p>"${song.author}"</p>
             `
+            div.addEventListener('click', () => {
+                console.log('Click sobre: ' + song.title)
+                    document.getElementById('audio')
+                        .setAttribute('src', song.path.audio)
+
+                        document.getElementById('front')
+                .setAttribute('src', song.path.front)
+        
+            })
+            // Finalment lo agregas al tracklist usando appendChild
+            tracklist.appendChild(div)
 
 
         })
