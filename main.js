5228b0d29b076ef272081ba08ff08a8a5476f76e
Leonardotorre18 <Leonardotorre14@gmail.com>
2023-10-23 21:30:21 -0400

    Ahora se usar AppendChild y CreateElement

--- main.js
+++ main.js
@@ -7,13 +7,33 @@ fetch('https://leonardoapi.onrender.com/music')
         
         resJson.map((song) => {
 
-            tracklist.innerHTML+=`
-            <div  class="box color">
-                    <img src="${song.path.front}" alt="">
-                    <h3>"${song.title}"</h3>
-                    <p>"${song.author}"</p>
-                  </div>
+            // tracklist.innerHTML+=`
+            //     <div  class="box color">
+            //         <img src="${song.path.front}" alt="">
+            //         <h3>"${song.title}"</h3>
+            //         <p>"${song.author}"</p>
+            //     +</div>
+            // `
+
+            // Recuerda que debes cambiar la manera de hacer el elemento
+
+            // Ahora usas createElement
+            const div = document.createElement('div')
+            div.classList.add(
+                'box',
+                'color'
+            )
+            div.innerHTML = `
+                <img src="${song.path.front}" alt="">
+                <h3>"${song.title}"</h3>
+                <p>"${song.author}"</p>
             `
+            div.addEventListener('click', () => {
+                // El evento lo creas dentro de este bloque de código
+                console.log('Click sobre: ' + song.title)
+            })
+            // Finalment lo agregas al tracklist usando appendChild
+            tracklist.appendChild(div)
 
 
         })
